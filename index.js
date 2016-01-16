4967c8870be04832cb97d19565b538d72f10b049
Ivan Zenteno <k001@users.noreply.github.com>
2016-01-16 07:38:54 -0600

    Adding witbot

--- index.js
+++ index.js
@@ -1,51 +1,56 @@
-var Botkit = require('botkit')
-
-// Expect a SLACK_TOKEN environment variable
-var slackToken = process.env.SLACK_TOKEN
-if (!slackToken) {
-  console.error('SLACK_TOKEN is required!')
-  process.exit(1)
+if (!process.env.SLACK_TOKEN || !process.env.WIT_TOKEN ) {
+    console.log('Error: Specify Slack or WIT token in environment');
+    process.exit(1);
 }
 
-var controller = Botkit.slackbot()
-var bot = controller.spawn({
-  token: slackToken
-})
+var Botkit = require('botkit');
+var Witbot = require('witbot');
+var os = require('os');
+var slackToken = process.env.SLACK_TOKEN;
+var witToken = process.env.WIT_TOKEN;
+var controller = Botkit.slackbot({
+    debug: false,
+    json_file_store: './db_slack',
+});
 
-bot.startRTM(function (err, bot, payload) {
-  if (err) {
-    throw new Error('Could not connect to Slack')
-  }
-})
+var bot = controller.spawn({
+    token: slackToken
+}).startRTM();
+
+var witbot = Witbot(witToken);
+
+controller.hears('.*', ['direct_message','direct_mention','mention','ambient'],function(bot, message){
+    witbot.process(message.text, bot, message);
+});
 
 controller.on('bot_channel_join', function (bot, message) {
-  bot.reply(message, "I'm here!")
-})
+  bot.reply(message, "I'm here!");
+});
 
 controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
-  bot.reply(message, 'Hello.')
-})
+  bot.reply(message, 'Hello.');
+});
 
 controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
-  bot.reply(message, 'Hello.')
-  bot.reply(message, 'It\'s nice to talk to you directly.')
-})
+  bot.reply(message, 'Hello.');
+  bot.reply(message, 'It\'s nice to talk to you directly.');
+});
 
 controller.hears('.*', ['mention'], function (bot, message) {
-  bot.reply(message, 'You really do care about me. :heart:')
-})
+  bot.reply(message, 'You really do care about me. :heart:');
+});
 
 controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
   var help = 'I will respond to the following messages: \n' +
       '`bot hi` for a simple message.\n' +
       '`bot attachment` to see a Slack attachment message.\n' +
       '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
-      '`bot help` to see this again.'
-  bot.reply(message, help)
-})
+      '`bot help` to see this again.';
+  bot.reply(message, help);
+});
 
 controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
-  var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
+  var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.';
   var attachments = [{
     fallback: text,
     pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
@@ -54,15 +59,15 @@ controller.hears(['attachment'], ['direct_message', 'direct_mention'], function 
     title_link: 'https://beepboophq.com/',
     text: text,
     color: '#7CD197'
-  }]
+  }];
 
   bot.reply(message, {
     attachments: attachments
   }, function (err, resp) {
-    console.log(err, resp)
-  })
-})
+    console.log(err, resp);
+  });
+});
 
 controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
-  bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
-})
+  bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n');
+});
